f82b4ca60bb89d8c490404d853d12ee417d493d2
LorenzoPratesi <pratesilorenzo@yahoo.it>
2021-06-04 17:45:31 +0200

    Initial import

---
+++ it.lorenzopratesi.app.hotelsbooking/src/main/java/it/lorenzopratesi/app/hotelsbooking/App.java
@@ -0,0 +1,13 @@
+package it.lorenzopratesi.app.hotelsbooking;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
